e1faa01a78e1ce56761f1000408890d8ff223404
groundstate <michaeljwouters@gmail.com>
2025-10-25 21:41:29 +1100

    Refactor. Don't interpolate - average.

--- butcgnss/butcgnss.py
+++ butcgnss/butcgnss.py
@@ -55,10 +55,10 @@ try:
 except ImportError:
 	sys.exit('ERROR: Must install rinexlib\n eg openttp/software/system/installsys.py -i rinexlib')
 	
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 AUTHORS = 'Michael Wouters'
 
-MIN_OBS = 5 # minimum number of observations for averaging
+REFSYS_AVG_WINDOW = 2 # window size for averaging
 
 # ------------------------------------------
 def ShowVersion():
@@ -122,73 +122,6 @@ def GetTimeSysCorr(gnss,navFile):
 				
 	fnav.close()
 
-# ------------------------------------------
-def GetLea(gnss,navFile):
-	
-	fnav = open(navFile,'r')
-	for l in fnav:
-		
-		if gnss == 'GPS':
-			if l[0:4] == 'GPUT': # covers version 2.12 ->
-				# Fields are offset a0, rate a1, tow, wn
-				return [float(l[5:22]), float(l[22:38]), int(l[38:45]),int(l[45:50])]
-			# TODO earlier RINEX 
-				
-	fnav.close()
-	
-# ---------------------------------------------
-# This finds the first 'good' track set, searching forwards from the start or backwards
-# from the end
-# TO DO currently this does not apply any quality checks
-# It just returns the first and the last
-
-def GetGoodTrackRange(startTrack,cgf,minObs):
-	trks = cgf.tracks
-	if startTrack == 0:
-		start = 0
-		stop = len(trks)
-		step = 1
-		sttime = trks[start][cgf.STTIME]
-		obsStart = start
-	elif startTrack == -1:
-		start = len(trks) - 1
-		stop = -1
-		step = -1
-		obsStop = start
-		sttime = trks[start][cgf.STTIME]
-	for i in range(start,stop,step):
-		if not (sttime == trks[i][cgf.STTIME]):
-			# Check that we got enough observations
-			if startTrack == 0:
-				if (i - obsStart) >= minObs:
-					return trks[obsStart][cgf.STTIME],obsStart,i-1
-				else:
-					obsStart = i # keep on going
-					sttime = trks[i][cgf.STTIME]
-			elif startTrack == -1:
-				if (obsStop - i) >= minObs:
-					return trks[i+1][cgf.STTIME],i+1,obsStop
-				else:
-					obsStop = i # keep on going
-					sttime = trks[i][cgf.STTIME]
-	return None,None,None
-	
-#----------------------------------------------
-# Calculate the average  value of REFSYS for the specified track range
-# TODO simple average - allow weighting
-#
-def GetRefSys(cgf,start,stop):
-	avg = 0
-	for i in range(start,stop+1):
-		ottp.Debug(f'{cgf.tracks[i][cgf.STTIME]} {cgf.tracks[i][cgf.REFSYS]}')
-		avg = avg + cgf.tracks[i][cgf.REFSYS]
-	avg = avg / (stop - start + 1)
-	
-	sumsq = 0
-	for i in range(start,stop+1):
-		sumsq = sumsq + (avg - cgf.tracks[i][cgf.REFSYS])*(avg - cgf.tracks[i][cgf.REFSYS])
-		
-	return avg,math.sqrt(sumsq/(stop - start))
 
 #----------------------------------------------
 def CheckConfig(cfg,req):
@@ -198,6 +131,72 @@ def CheckConfig(cfg,req):
 			print(f'{r} is not set')
 			ok = False
 	return ok
+
+# ---------------------------------------------
+def GetRefsys(cgBef,cgAft,winSize):
+
+	wSz = 1800*winSize 
+	refsys0 = None
+	uRefsys0 = None
+	nTracks = 0
+	
+	tBef = tAft = 0
+	nBef = nAft = 0
+
+	# Seems messy compared with a concatenating tha data into a single array
+	# it does allow the two CGGTTS files to have different formats
+	
+	if cgBef.fileName:
+		tStart = 86400 - wSz
+		ntrks = len(cgBef.tracks)
+		for t in range(0,ntrks):
+			if cgBef.tracks[t][cgBef.STTIME] >= tStart: # don't worry about the 390 s offset
+				tBef = t 
+				nBef = ntrks - tBef
+				break
+				
+	if cgAft.fileName:
+		tStop = wSz
+		ntrks = len(cgBef.tracks)
+		for t in range(0,ntrks):
+			if cgAft.tracks[t][cgBef.STTIME] >= tStop:
+				tAft = t - 1
+				nAft = tAft + 1
+				break
+		
+	if not(nBef and nAft):
+		return refsys0,uRefsys0,nTracks
+	
+	refsys0 = 0
+	
+	if nBef > 0:
+		ntrks = len(cgBef.tracks)
+		for t in range(tBef,ntrks):
+			#print(cgBef.tracks[t][cgBef.STTIME],cgBef.tracks[t][cgBef.REFSYS])
+			refsys0 = refsys0 + cgBef.tracks[t][cgBef.REFSYS]
+	if nAft > 0:
+		ntrks = len(cgAft.tracks)
+		for t in range(0,tAft+1):
+			#print(cgAft.tracks[t][cgAft.STTIME],cgAft.tracks[t][cgAft.REFSYS])
+			refsys0 = refsys0 + cgAft.tracks[t][cgAft.REFSYS]
+	
+	nTracks = nBef + nAft
+	if nTracks == 1: # wayy... too little
+		return refsys0,uRefsys0,nTracks
+		
+	refsys0 = refsys0/nTracks 
+	
+	urefsys0 = 0
+	if nBef > 0:
+		ntrks = len(cgBef.tracks)
+		for t in range(tBef,ntrks):
+			urefsys0 += (refsys0 - cgBef.tracks[t][cgBef.REFSYS])**2
+	if nAft > 0:
+		ntrks = len(cgAft.tracks)
+		for t in range(0,tAft+1):
+			urefsys0 += (refsys0  - cgAft.tracks[t][cgAft.REFSYS])**2
+	
+	return refsys0,math.sqrt(urefsys0/(nTracks-1)),nTracks
 
 # ---------------------------------------------
 def formatNumber(fval):
@@ -216,6 +215,8 @@ rnxDir = os.path.join(root,'rinex')
 tmpDir  = os.path.join(root,'tmp')
 reportDir = os.path.join(root,'reports')
 cggttsDir = os.path.join(root,'cggtts')
+
+winSize = REFSYS_AVG_WINDOW
 
 if ottp.LibMajorVersion() >= 0 and ottp.LibMinorVersion() < 2: 
 	print('ottplib minor version < 2')
@@ -267,16 +268,16 @@ ts = time.time() - 86400 # previous day
 stopMJD = ottp.MJD(ts) 
 dt = datetime.fromtimestamp(ts, tz=timezone.utc) # get date in UTC
 ts   = ts - 86400*(dt.day-1) # FIXME check month rollover
-startMJD = ottp.MJD(ts)-1 # need to get the previous day for interpolation
+startMJD = ottp.MJD(ts)-1 # need to get the previous day 
 auto = True # i
 ottp.Debug(f'Default processing range {startMJD} - {stopMJD}')
 
 if (args.mjd):
 	if 1 == len(args.mjd):
-		startMJD = int(args.mjd[0]) - 1 # need to get the previous day for interpolation 
-		stopMJD  = startMJD
+		startMJD = int(args.mjd[0]) - 1 # need to get the previous day 
+		stopMJD  = int(args.mjd[0])        # 
 	elif ( 2 == len(args.mjd)):
-		startMJD = int(args.mjd[0]) - 1 # need to get the previous day for interpolation 
+		startMJD = int(args.mjd[0]) - 1 # need to get the previous day 
 		stopMJD  = int(args.mjd[1])
 		if (stopMJD < startMJD):
 			ottp.ErrorExit('Stop MJD is before start MJD')
@@ -287,9 +288,8 @@ if (args.mjd):
 
 # First pass - update UTC(k) - bUTC_GNSS
 freport = None # initially, the report file is closed or doesn't yet exist
-
-prevRefsys = {}
-prevRefsys['GPS'] = []
+prevCGGTTSFile = {}
+prevCGGTTSFile['GPS'] = CGGTTS(None,None)
 
 for mjd in range(startMJD,stopMJD + 1):
 	
@@ -324,79 +324,48 @@ for mjd in range(startMJD,stopMJD + 1):
 			ottp.Debug(f'Reading {fname}')
 			cgf = CGGTTS(fname,mjd)
 			cgf.Read()
-			
-			sttFirst,start,stop = GetGoodTrackRange(0,cgf,MIN_OBS)  # search forwards from the start
-			if not(start == None) and not(stop==None):
-				rsFirst,ursFirst = GetRefSys(cgf,start,stop)
-				ottp.Debug(f'[{start} {stop}] avg = {rsFirst} std = {ursFirst}')
-			else:
-				rsFirst = None
-				ursFirst = None
-				
-			sttLast,start,stop = GetGoodTrackRange(-1,cgf,MIN_OBS) # search backwards from the end
-			if not(start == None) and not(stop==None):
-				rsLast,ursLast = GetRefSys(cgf,start,stop)
-				ottp.Debug(f'[{start} {stop}] avg = {rsLast} std = {ursLast}')
-			else:
-				rsLast = None
-				ursLast = None
 		else:
 			ottp.Debug('No CGGTTS file found')
-			sttFirst = None
-			rsFirst  = None
-			ursFirst = None
-			rsLast   = None
-			ursLast  = None
-			sttLast  = None
+			cgf = CGGTTS(None,mjd)
 		
-		# The first one is just so we get REFSYS at the end of the day for interpolation
-		# so no more to do
+		# The first one is for the day before the start MJD so no more to do
 		if (mjd==startMJD):
-			prevRefsys[g] = [sttLast,rsLast,ursLast] # save the REFSYS value
+			prevCGGTTSFile[g] = cgf # save it 
 			continue
 		
-		# Interpolate, if we can
-		if not (rsFirst == None) and not(prevRefsys[g][0]  == None):
-			#print(prevRefsys[g][0],prevRefsys[g][1],sttFirst,rsFirst)
-			# If y = mt + y0
-			# then we want y = y0
-			# y0 = y2  - t2*(y2 - y1)/(t2-t1) 
-			refsys0 = rsFirst - sttFirst*(rsFirst - prevRefsys[g][1] )/(sttFirst - (prevRefsys[g][0]-86400))
-			# Calculate the uncertainty due to the interpolation
-			# y0 = (1 - t2/(t2-t1))*y2 + (t2/(t2-t1))*y1
-			#    = (1  - c)*y2 + c* y1 
-			# so 
-			#   u = sqrt((1-c)^2 * uy2^2 + c^2 * uy1^2)
-			c = sttFirst/(sttFirst - (prevRefsys[g][0]-86400))
-			urefsys0 = math.sqrt((1-c)*(1-c)*ursFirst*ursFirst + c*c*prevRefsys[g][2])
-			#print(mjd, refsys0,urefsys0)
+		# Calculate the average REFSYS, if we can
+		if prevCGGTTSFile[g].fileName or cgf.fileName:
+			refsys0,uRefsys0,nTracks = GetRefsys(prevCGGTTSFile[g],cgf,winSize);
+			print(refsys0,uRefsys0,nTracks)
 		
-			# Find the navigation file
-			navFile,zExt = rinex.FindNavigationFile(rnxDir,staName,yyyy,doy,rnxVersion,False) # don't exit if not found
+			if not (refsys0 == None):
+				# Find the navigation file
+				navFile,zExt = rinex.FindNavigationFile(rnxDir,staName,yyyy,doy,rnxVersion,False) # don't exit if not found
 
-			if navFile:
-				ottp.Debug(f'Found {navFile}, compression = {zExt}')
-				src = navFile + zExt
-				srcBase = os.path.basename(src)
-				dst = os.path.join(tmpDir,srcBase)
-				# We may not own the file so we need to make a local copy
-				shutil.copy(src,dst)
-				navFile,zAlgorithm = rinex.Decompress(dst)
-				tsCorr = GetTimeSysCorr(g,navFile)
-				leapSecs =rinex.GetLeapSeconds(navFile,rnxVersion)
-				if g == 'GPS':
-					# From the ICD
-					# t_UTC = t_E - delta_UTC where t_E is 'effective' GPS time 
-					# delta_UTC = dt_LS + A0 + A1*(t_E - t0t + 604800*(WN - Wn_t) )
-					# We want the value at UTC0 for the day which means
-					# t_E = 86400*gpsDn + leapSecs
-					deltaUTC = tsCorr[0] + tsCorr[1]*(86400*gpsDn + leapSecs - tsCorr[2] + 604800*(gpsWn - tsCorr[3]))
-					# print(deltaUTC,tsCorr,gpsDn,gpsWn,leapSecs)
-					reportData[g][0] = refsys0 + deltaUTC*1.0E9
-					reportData[g][1] = urefsys0
-			
+				if navFile:
+					ottp.Debug(f'Found {navFile}, compression = {zExt}')
+					src = navFile + zExt
+					srcBase = os.path.basename(src)
+					dst = os.path.join(tmpDir,srcBase)
+					# We may not own the file so we need to make a local copy
+					shutil.copy(src,dst)
+					navFile,zAlgorithm = rinex.Decompress(dst)
+					tsCorr = GetTimeSysCorr(g,navFile)
+					leapSecs =rinex.GetLeapSeconds(navFile,rnxVersion)
+					if g == 'GPS':
+						# From the ICD
+						# t_UTC = t_E - delta_UTC where t_E is 'effective' GPS time 
+						# delta_UTC = dt_LS + A0 + A1*(t_E - t0t + 604800*(WN - Wn_t) )
+						# We want the value at UTC0 for the day which means
+						# t_E = 86400*gpsDn + leapSecs
+						deltaUTC = tsCorr[0] + tsCorr[1]*(86400*gpsDn + leapSecs - tsCorr[2] + 604800*(gpsWn - tsCorr[3]))
+						# print(deltaUTC,tsCorr,gpsDn,gpsWn,leapSecs)
+						reportData[g][0] = refsys0 + deltaUTC*1.0E9
+						reportData[g][1] = uRefsys0
+		else:
+			ottp.Debug('Insufficent CGGTTS data')
 	
-		prevRefsys[g] = [sttLast,rsLast,ursLast] # save REFSYS values from the end of day for the next iteration
+		prevCGGTTSFile[g] = cgf # save it for the next time
 		
 	# If we can't generate the data, mark as missing'
 	outputLine = f'{mjd:5d}'
